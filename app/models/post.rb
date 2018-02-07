93506556139fd2a294e6e6b435c64916861e19a1
Min Kim <mk4555@nyu.edu>
2018-02-07 20:15:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 100}
   validates :summary, length: {maximum: 10}
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
-end
 
+  def not_click_bait
+  end
+end
