81ec16cb6ce21f6c11602c644cbc052d7cd44232
Min Kim <mk4555@nyu.edu>
2018-02-07 20:18:59 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
 
   def not_click_bait
-    title.present? && title.include?("True Facts")
+    title.present? && !title.include?("True Facts")
   end
 end
